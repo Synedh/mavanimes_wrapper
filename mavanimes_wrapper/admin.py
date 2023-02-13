d3fcba16e6d12c31a4e142998c924a67cf35c08f
Synedh <contact@synedh.fr>
2023-02-13 14:37:11 +0000

    Html span enhancing and minor corrections.

--- mavanimes_wrapper/admin.py
+++ mavanimes_wrapper/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 
-admin.site.site_header = "Administration de Mavanimes Wrapper"
+admin.site.site_header = "Mavanimes Wrapper - Admin"
 admin.site.index_title = "Administration"
