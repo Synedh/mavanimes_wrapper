f7994cc194bc85eb58da4cba45a7c404249f7095
Synedh <contact@synedh.fr>
2023-10-18 22:43:10 +0200

    Removed unused function.

--- utils/utils.py
+++ utils/utils.py
@@ -2,9 +2,7 @@ import logging
 from datetime import datetime
 from typing import Optional, TypedDict
 
-from django.template.defaultfilters import slugify
-
-from apps.animes.models import Anime, Episode, VideoURL
+from apps.animes.models import Anime, Episode
 
 logger = logging.getLogger(__name__)
 
@@ -20,35 +18,6 @@ class EpisodeDTO(TypedDict):
     pub_date: datetime
     video_urls: list[str]
     mav_url: str
-
-
-def save_episode(episode_dict: EpisodeDTO) -> Episode:
-    anime, new_anime = Anime.objects.get_or_create(slug=slugify(episode_dict['anime']))
-    if new_anime:
-        anime.name = episode_dict['anime']
-
-    video_urls = episode_dict['video_urls']
-    del episode_dict['video_urls']
-    episode_dict['anime'] = anime
-
-    if new_anime:
-        logger.info('New anime %s', anime.name)
-
-    episode, new_episode = Episode.objects.update_or_create(
-        name=episode_dict['name'], anime=anime.id,
-        defaults={**episode_dict}
-    )
-
-    previous_video_urls = [video_url.url for video_url in episode.video_urls.all()]
-    if video_urls != previous_video_urls:
-        episode.video_urls.all().delete()
-        _ = [VideoURL.objects.get_or_create(
-            url=url,
-            source=url.split('.')[0].split('/')[-1],
-            episode=episode
-        ) for url in video_urls]
-    logger.info('%s episode %s', "New" if new_episode else "Updated", episode.name)
-    return episode
 
 
 def move_anime(old_id: int, new_id: int, season: int | None=1, delete: bool | None=False) -> None:
